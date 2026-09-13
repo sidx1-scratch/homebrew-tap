7e0e43bb3496bfa34c51d4dde93875073cb70530
sidx1-scratch <sid9ihd@gmail.com>
2026-09-12 18:05:14 -0600

    Update sure formula v0.1.0 with official release SHA256 checksums

--- Formula/sure.rb
+++ Formula/sure.rb
@@ -7,23 +7,22 @@ class Sure < Formula
   on_linux do
     on_intel do
       url "https://github.com/sidx1-scratch/sure/releases/download/v0.1.0/sure_0.1.0_linux_amd64.tar.gz"
-      # sha256 will need to be updated after release artifacts are built
-      sha256 "PLACEHOLDER"
+      sha256 "91420abf64c42c0532393dabd211b2e9deb030e4296887797a673573d4cb86d9"
     end
     on_arm do
       url "https://github.com/sidx1-scratch/sure/releases/download/v0.1.0/sure_0.1.0_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "ce9019473b737ecdc43be2a95d2cb2ee275e6dfb0ebad964111ca8a8c153a510"
     end
   end
 
   on_macos do
     on_intel do
       url "https://github.com/sidx1-scratch/sure/releases/download/v0.1.0/sure_0.1.0_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "1e525d79a86aab1c5c94ef2305941df5a9d208cb1e0323d1e8acd9ab77103617"
     end
     on_arm do
       url "https://github.com/sidx1-scratch/sure/releases/download/v0.1.0/sure_0.1.0_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "4531c6e802dbeeb0f96a96ad5429ce0e9b7feced46b6c9c00efd69a316be02b5"
     end
   end
 
